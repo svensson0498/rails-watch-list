f8d93fd3804bb474388bc6e3d6a06a4149897fd8
Mikael Svensson <svensson_0498@hotmail.com>
2021-05-21 15:12:24 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 resources :bookmarks, only: [:destroy]
 resources :lists do
     resources :bookmarks, only: [:new, :create,]
-  end
+end
+root to: 'lists#index'
 end
