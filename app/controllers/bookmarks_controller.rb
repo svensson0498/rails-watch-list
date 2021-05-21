412a497ca5934d9a3048ee620a5a8b219669c5cf
Mikael Svensson <svensson_0498@hotmail.com>
2021-05-21 15:38:49 +0200

    update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -21,7 +21,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
     @list = @bookmark.list
     @bookmark.destroy
-    redirect_to list_path
+    redirect_to list_path(@list)
   end
 
   private
